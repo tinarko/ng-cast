5e0fe57c34f84496680a1cb3f57792086fb73f77
Apurva Shastry <apurva.shastry@gmail.com>
2017-02-27 20:08:18 -0800

    All tests are now green

--- src/components/search.js
+++ src/components/search.js
@@ -1,17 +1,17 @@
 angular.module('video-player')
 .controller('SearchCtrl', function() {
   this.clickHandler = () => {
-  	this.service.search(this.input, (data) => {
+    this.service.search(this.input, (data) => {
       this.result(data);
     });
-  }
+  };
 })
 .directive('search', function() {
   return {
-  	scope: {
-  		service: '<',
-  		result: '<'
-  	},
+    scope: {
+      service: '<',
+      result: '<'
+    },
     controller: 'SearchCtrl',
     controllerAs: 'ctrl',
     bindToController: true,
